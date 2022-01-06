4ad97c644846a9e67a577f262ed932091c1f951f
David Leiter <david.leiter@tradokk.com>
2022-01-06 12:28:09 +0100

    reformat lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,36 +1,31 @@
 return require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
-	use { 
-		'nvim-lualine/lualine.nvim',
-  		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-	}
-	use 'williamboman/nvim-lspconfig'
-	use 'williamboman/nvim-lsp-installer'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
-	use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-	}
-	use 'Mofiqul/vscode.nvim'
-	use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
-	use {
-	  'nvim-telescope/telescope.nvim',
-	  requires =  {'nvim-lua/plenary.nvim'} 
-	}
-	use 'nvim-telescope/telescope-fzy-native.nvim'
-	use 'L3MON4D3/LuaSnip'
-	use 'ray-x/lsp_signature.nvim'
-	use {
-		'kyazdani42/nvim-tree.lua',
-		requires = {'kyazdani42/nvim-web-devicons'}}
-	use 'akinsho/toggleterm.nvim'
-	-- use 'mfussenegger/nvim-dap'
-	-- use 'Pocco81/DAPInstall.nvim'
-	-- use 'Shatur/neovim-cmake'
-	use { 'tami5/lspsaga.nvim' }
-	use 'rmagatti/auto-session'
-	end)
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
+    use 'williamboman/nvim-lspconfig'
+    use 'williamboman/nvim-lsp-installer'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use 'Mofiqul/vscode.nvim'
+    use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+    use {'nvim-telescope/telescope.nvim', requires = {'nvim-lua/plenary.nvim'}}
+    use 'nvim-telescope/telescope-fzy-native.nvim'
+    use 'L3MON4D3/LuaSnip'
+    use 'ray-x/lsp_signature.nvim'
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = {'kyazdani42/nvim-web-devicons'}
+    }
+    use 'akinsho/toggleterm.nvim'
+    -- use 'mfussenegger/nvim-dap'
+    -- use 'Pocco81/DAPInstall.nvim'
+    -- use 'Shatur/neovim-cmake'
+    use {'tami5/lspsaga.nvim'}
+    use 'rmagatti/auto-session'
+end)
--- lua/utils.lua
+++ lua/utils.lua
@@ -1,19 +1,19 @@
-function nmap(lhs,rhs)
-	vim.api.nvim_set_keymap('n',lhs,rhs,{noremap = false, silent=true})
+function nmap(lhs, rhs)
+    vim.api.nvim_set_keymap('n', lhs, rhs, {noremap = false, silent = true})
 end
 
-function noremap(lhs,rhs)
-	vim.api.nvim_set_keymap('',lhs,rhs, { noremap = true , silent=true} )
+function noremap(lhs, rhs)
+    vim.api.nvim_set_keymap('', lhs, rhs, {noremap = true, silent = true})
 end
 
-function nnoremap(lhs,rhs)
-	vim.api.nvim_set_keymap('n',lhs,rhs, { noremap = true, silent=true} )
+function nnoremap(lhs, rhs)
+    vim.api.nvim_set_keymap('n', lhs, rhs, {noremap = true, silent = true})
 end
 
-function tnoremap(lhs,rhs)
-	vim.api.nvim_set_keymap('t',lhs,rhs, { noremap = true, silent=true} )
+function tnoremap(lhs, rhs)
+    vim.api.nvim_set_keymap('t', lhs, rhs, {noremap = true, silent = true})
 end
 
-function xnoremap(lhs,rhs)
-	vim.api.nvim_set_keymap('x',lhs,rhs, { noremap = true, silent=true} )
+function xnoremap(lhs, rhs)
+    vim.api.nvim_set_keymap('x', lhs, rhs, {noremap = true, silent = true})
 end
