77e709f8206cc7f4f3d74af5415d02c593b84a82
David Leiter <davidleiter92@gmail.com>
2022-11-27 19:24:36 +0100

    Update Plugins

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -70,9 +70,8 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp
-                                                                     .protocol
-                                                                     .make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 -- local servers = {'clangd','pyright'}
 -- for _, server in ipairs(servers) do 
 --	  lspconfig[server].setup({
