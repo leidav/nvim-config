df8eae1a4d1a36091e2c50f1c21937352360e8d6
David Leiter <davidleiter92@gmail.com>
2023-01-28 23:17:22 +0100

    use native lua key mapping functions

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,19 +1,19 @@
 function nmap(lhs, rhs)
-    vim.api.nvim_set_keymap('n', lhs, rhs, {noremap = false, silent = true})
+    vim.keymap.set('n', lhs, rhs, {noremap = false, silent = true})
 end
 
 function noremap(lhs, rhs)
-    vim.api.nvim_set_keymap('', lhs, rhs, {noremap = true, silent = true})
+    vim.keymap.set('', lhs, rhs, {noremap = true, silent = true})
 end
 
 function nnoremap(lhs, rhs)
-    vim.api.nvim_set_keymap('n', lhs, rhs, {noremap = true, silent = true})
+    vim.keymap.set('n', lhs, rhs, {noremap = true, silent = true})
 end
 
 function tnoremap(lhs, rhs)
-    vim.api.nvim_set_keymap('t', lhs, rhs, {noremap = true, silent = true})
+    vim.keymap.set('t', lhs, rhs, {noremap = true, silent = true})
 end
 
 function xnoremap(lhs, rhs)
-    vim.api.nvim_set_keymap('x', lhs, rhs, {noremap = true, silent = true})
+    vim.keymap.set('x', lhs, rhs, {noremap = true, silent = true})
 end
