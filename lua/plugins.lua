402ed2bbc529dbf4767fda8b0a8661ad647a26f2
David Leiter <davidleiter92@gmail.com>
2023-01-28 23:17:59 +0100

    add neodev to plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -30,4 +30,5 @@ return require('packer').startup(function()
     use 'onsails/lspkind-nvim'
     use {'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim'}
     use 'lukas-reineke/indent-blankline.nvim'
+	use 'folke/neodev.nvim'
 end)
